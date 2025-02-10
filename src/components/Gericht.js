36efd903f930d340de8fe1a6e7d5ad761853336c
Jan Sebald <j.sebald@gmx.net>
2025-02-10 19:59:31 +0100

    Bildpfad korrigiert

--- src/components/Gericht.js
+++ src/components/Gericht.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Gericht({ name, preis, bild }) {
   return (
     <div className="gericht">
-      <img src={`./images/${bild}`} alt={name} className="gericht-bild" />
+<img src={`${process.env.PUBLIC_URL}/images/${bild}`} alt={name} className="gericht-bild" />
       <div className="gericht-info">
         <h3>{name}</h3>
         <p>{preis}</p>
