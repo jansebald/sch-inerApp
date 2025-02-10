7f730c5256f28602a61a18b405d8c437b992deaa
Jan Sebald <j.sebald@gmx.net>
2025-02-10 20:03:00 +0100

    Bilder direkt importiert

--- src/components/Gericht.js
+++ src/components/Gericht.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import currywurstBild from '../images/currywurst.jpg';  // Bild importieren
 
 function Gericht({ name, preis, bild }) {
+  let imagePath;
+  
+  // Falls du das Bild dynamisch setzen möchtest, kannst du die Pfade hier importieren
+  if (bild === 'currywurst.jpg') {
+    imagePath = currywurstBild;
+  }
+  else {
+    // Hier kannst du weitere Bilder importieren
+    imagePath = require(`../images/${bild}`).default;
+  }
+
   return (
     <div className="gericht">
-<img src={`${process.env.PUBLIC_URL}/images/${bild}`} alt={name} className="gericht-bild" />
+      <img src={imagePath} alt={name} className="gericht-bild" />
       <div className="gericht-info">
         <h3>{name}</h3>
         <p>{preis}</p>
